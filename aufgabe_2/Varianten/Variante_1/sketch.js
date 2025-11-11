67728b51d2843319694cf9e952539904fbdf3f02
Gin-To <gina.toschi@stud.sfgz.ch>
2025-11-11 19:23:11 +0100

    Varianten

--- aufgabe_2/Varianten/Variante_1/sketch.js
+++ aufgabe_2/Varianten/Variante_1/sketch.js
@@ -1,10 +1,12 @@
 let x;         // x-Position des Vierecks
 let angle = 0; // Drehwinkel
+let rectColor; // Farbe des Rechtecks
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
   rectMode(CENTER);
   x = width / 2; // Start in der Mitte
+  rectColor = color(255, 0, 0); // Anfangsfarbe Rot
 }
 
 function draw() {
@@ -12,19 +14,28 @@ function draw() {
 
   // Bewegung & Rotation aktualisieren
   x += 2;           // gehe nach rechts
-  angle += PI / 180; // drehe dich langsam (90° = PI/2)
+  angle += PI / 180; // drehe dich langsam
 
   // Zeichnungstransformationen
-  push(); 
-  translate(x, height / 2); // Position verschieben
-  rotate(angle);            // drehen
-  fill(100, 0, 100);
-  rect(0, 0, 100, 100);     // Viereck zeichnen
+  push();
+  translate(x, height / 2);
+  rotate(angle);
+  fill(rectColor);     // Rechteck mit aktueller Farbe füllen
+  rect(0, 0, 100, 100);
   pop();
 
   // Wenn das Viereck aus dem Bild ist, neu starten
   if (x - 50 > width) {
-    x = -50;      // starte links wieder
-    angle = 0;    // Winkel zurücksetzen
+    x = -50;
+    angle = 0;
   }
+}
+
+// Diese Funktion MUSS außerhalb von draw() stehen
+function mousePressed() {
+  rectColor = color(
+    random(255),
+    random(255),
+    random(255)
+  );
 }
