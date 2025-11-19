d1bb87b6fbd578a1c091398687a40e5b49806c42
Gin-To <gina.toschi@stud.sfgz.ch>
2025-11-19 16:25:13 +0100

    präsi

--- aufgabe_1/abgabe_30_10/sketch.js
+++ aufgabe_1/abgabe_30_10/sketch.js
@@ -11,7 +11,7 @@ function setup() {
 }
 
 function draw() {
-  
+
   let val = valueSlider.value();
 
   // Farben
@@ -23,23 +23,23 @@ function draw() {
   fill(dark);
   rect(0, 0, width / 2, height);
 
-//Position width/2 ist oben in der Mitte
+  //Position width/2 ist oben in der Mitte
   fill(light);
   rect(width / 2, 0, width / 2, height);
 
-  // Slider steuert Grösse gegengleich
+  //Slider steuert Grösse gegengleich
   //Syntax: map(value, start1, stop1, start2, stop2) übersetzt value von Bereich [start1, stop1] linear in den Bereich [start2, stop2].
   //Wenn val = 0 → leftDiameter = maxDiameter (800).
   let leftDiameter = map(val, 0, 100, maxDiameter, minDiameter);
   let rightDiameter = map(val, 0, 100, minDiameter, maxDiameter);
 
 
-//midX ist die x-Koordinate der vertikalen Mittellinie, centerY die Y-Koordinate
-// Kreise in der Mitte des Canvas zeichnen
+  //midX ist die x-Koordinate der vertikalen Mittellinie, centerY die Y-Koordinate
+  // Kreise in der Mitte des Canvas zeichnen
   let midX = width / 2;
   let centerY = height / 2;
 
-  // Sicherstellen, dass die Kreise nicht über den Rand hinausgehen
+  // Sicherstellen, dass die Kreise nicht über die mittellinie gehen
   let leftX = max(leftDiameter / 2, midX - leftDiameter / 2); // links im Feld
   let rightX = min(width - rightDiameter / 2, midX + rightDiameter / 2); // rechts im Feld
 
