21f63a821ed8d1e43dc9eb0590413e02330e312e
Gin-To <gina.toschi@stud.sfgz.ch>
2025-11-19 13:55:44 +0100

    Präsi vorbereiten

--- aufgabe_1/abgabe_30_10/sketch.js
+++ aufgabe_1/abgabe_30_10/sketch.js
@@ -1,7 +1,8 @@
 let valueSlider;
-let maxDiameter = 800; // maximaler Durchmesser
-let minDiameter = 20;  // minimaler Durchmesser
+let maxDiameter = 800; // maximaler Durchmesser Kreis
+let minDiameter = 20;  // minimaler Durchmesser Kreis
 
+//Slider im Stylesheet nach unten in der mitte positioniert
 function setup() {
   createCanvas(windowWidth, windowHeight);
   valueSlider = createSlider(0, 100, 50);
@@ -18,16 +19,23 @@ function draw() {
   let light = color(242, 189, 205);
 
   // Hintergrundhälften
+  //Position 0,0 oben links
   fill(dark);
   rect(0, 0, width / 2, height);
 
+//Position width/2 ist oben in der Mitte
   fill(light);
   rect(width / 2, 0, width / 2, height);
 
   // Slider steuert Grösse gegengleich
+  //Syntax: map(value, start1, stop1, start2, stop2) übersetzt value von Bereich [start1, stop1] linear in den Bereich [start2, stop2].
+  //Wenn val = 0 → leftDiameter = maxDiameter (800).
   let leftDiameter = map(val, 0, 100, maxDiameter, minDiameter);
   let rightDiameter = map(val, 0, 100, minDiameter, maxDiameter);
 
+
+//midX ist die x-Koordinate der vertikalen Mittellinie, centerY die Y-Koordinate
+// Kreise in der Mitte des Canvas zeichnen
   let midX = width / 2;
   let centerY = height / 2;
 
